80bbfafebb1c3f1520b187295729d6d8455af88f
jeffrey vierra <st.pious@gmail.com>
2017-07-20 15:38:05 -1000

    Added if statements
    2 if statements were co.citation & co.year to check for null or
    undefined fields.

--- js/playlist.js
+++ js/playlist.js
@@ -11,3 +11,28 @@ function print(message) {
   document.write(message);
 }
 
+
+
+function printList (list ){
+
+	var listHTML = '<ol>';
+	for (var i = 0; i < list.length; i +=1 ) {
+		listHTML += '<li>' + list[i] + '</li>';
+	}
+
+  listHTML += '</ol>'
+  print(listHTML);
+
+}
+
+
+printList(playList);
+
+
+
+
+
+
+
+
+
